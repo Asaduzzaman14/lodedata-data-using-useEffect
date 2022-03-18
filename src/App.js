04135762a5682724b5270ccb61ec03511b0940f3
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-18 15:45:22 +0600

    lode data and complete daynamic data show

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import Country from './component/country/Country';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bd devision</h1>
+      <LoadPost></LoadPost>
+
+
+      <Country name='Chittagong' dist={'khagrachhary'}></Country>
+      <Country name={'Chittagong'} dist={'Bandarban'}></Country>
+      <Country name={'Chittagong'} dist={'nowakhali'}></Country>
     </div>
   );
+};
+
+function LoadPost() {
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+  return (
+    <div>
+      <h1>post:</h1>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/component/country/Country.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './Country.css'
+const Country = (props) => {
+    const [count, setCount] = useState(1)
+
+    const boostPower = () => {
+        const newPewer = count * 2
+        setCount(newPewer)
+    }
+
+    return (
+        <div className='distric'>
+            <h2>Vivag name: {props.name}</h2>
+            <h3>Distric: {props.dist}</h3>
+            <p>power :{count}</p>
+            <button onClick={boostPower} className='btn'>Bost Power  </button>
+
+        </div>
+    );
+};
+
+export default Country;
